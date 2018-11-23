ea8fc7ab0037388d2a794b7a55666d4f9c78897b
Mathieu Tanneau <mathieu.tanneau@gmail.com>
2018-11-23 15:58:54 -0500

    Run experiments from single function call

--- exp/experiments.jl
+++ exp/experiments.jl
@@ -7,36 +7,13 @@ using BenchmarkTools
 using SparseArrays
 using Printf
 
-import MathProgBase
-const MPB = MathProgBase
-
-import CPLEX:CplexSolver
-import Gurobi: GurobiSolver
-import Mosek: MosekSolver
-
-import Linda
-import Tulip
 BLAS.set_num_threads(1)
-
-# Extend MPB 
-import Mosek
-import Gurobi
-import CPLEX
-
-MPB.getbarrieriter(m::Mosek.MosekMathProgSolverInterface.MosekLinearQuadraticModel) = Mosek.getintinf(m.task, Mosek.MSK_IINF_INTPNT_ITER)
-MPB.getsimplexiter(m::Mosek.MosekMathProgSolverInterface.MosekLinearQuadraticModel) = Mosek.getintinf(m.task,Mosek.MSK_IINF_SIM_PRIMAL_ITER)+Mosek.getintinf(m.task,Mosek.MSK_IINF_SIM_DUAL_ITER)
-
-MPB.getsimplexiter(m::Tulip.TulipMathProgModel) = 0
-
-MPB.getsimplexiter(m::Gurobi.GurobiMathProgModel) = Gurobi.get_dblattr(m.inner, "IterCount")
-MPB.getbarrieriter(m::Gurobi.GurobiMathProgModel) = Gurobi.get_intattr(m.inner, "BarIterCount")
-
-MPB.getbarrieriter(m::CPLEX.CplexMathProgModel) = CPLEX.@cpx_ccall(getbaritcnt, Cint, (Ptr{Cvoid}, Ptr{Cvoid}), m.inner.env, m.inner.lp)
-MPB.getsimplexiter(m::CPLEX.CplexMathProgModel) = CPLEX.@cpx_ccall(getitcnt, Cint, (Ptr{Cvoid}, Ptr{Cvoid}), m.inner.env, m.inner.lp)
 
 # Demand Response module
 include("../src/DemandResponse.jl")
-# const DR = DemandResponse
+DR = DemandResponse
+
+include("solvers.jl")
 
 """
     generate_mp(n, T, netloadmin, netloadmax, cols, lpsolver)
@@ -191,104 +168,139 @@ function generate_resources(
         appliances = DR.DER.Resource[]
         γ = 0.5 + rand()  # household's scaling factor
 
-        # Fixed load
-        l = DR.DER.FixedLoad(
-            index=i,
-            T=T,
-            load= γ .* max.(0.0, (load_norm .+ 0.05 .* randn(T)))
-        )
-        push!(appliances, l)
-
-        # Shiftable loads
-        # Dishwasher
-        dishwasher = DR.DER.ShiftableLoad(
-            index = 10000000 + i,  # TODO: handle index number properly
-            T=T,
-            cycle_begin_min=1,
-            cycle_begin_max=T-1,
-            cycle_length=2,
-            cycle_load=[1.0, 1.0]
-        )
-        push!(appliances, dishwasher)
-
-        clothes_washer = DR.DER.ShiftableLoad(
-            index = 20000000 + i,  # TODO: handle index number properly
-            T=T,
-            cycle_begin_min=1,
-            cycle_begin_max=T-1,
-            cycle_length=2,
-            cycle_load=[1.0, 1.0]
-        )
-        push!(appliances, clothes_washer)
-
-        clothes_dryer = DR.DER.ShiftableLoad(
-            index = 30000000 + i,  # TODO: handle index number properly
-            T=T,
-            cycle_begin_min=1,
-            cycle_begin_max=T-2,
-            cycle_length=3,
-            cycle_load=[1.0, 1.0, 1.0]
-        )
-        push!(appliances, clothes_dryer)
-
-        # Thermal load
-        th = DR.DER.ThermalLoad(
-            index=i, T=T, dt=1.0,
-            temp_min=fill(18.0, T), temp_max=fill(22.0, T),
-            temp_ext=temp_ext + 0.5 .* randn(T),
-            pwr_min=zeros(T), pwr_max=10.0 .* ones(T),
-            C=3.0, η=1.0, μ=0.2,
-            binary_flag=false
-        )
-        
-        # Electric vehicle
-        if (T % 24) == 0
-            ndays = div(T, 24)
-
-            ev = DR.DER.DeferrableLoad(
-                index=i, T=T, dt=1.0,
-                num_cycles=ndays,
-                cycle_begin=[16 + 24*(d-1) for d in 1:ndays],
-                cycle_end=[24 + 24*(d-1) for d in 1:ndays],
-                cycle_energy_min=fill(10.0, ndays),
-                cycle_energy_max=fill(10.0, ndays),
-                pwr_min=repeat([zeros(15); 1.1*ones(9)], ndays),
-                pwr_max=repeat([zeros(15); 7.7*ones(9)], ndays),
-                binary_flag=true
+        # ==================
+        #    Fixed load     
+        # ==================
+        # All houses have a fixed load
+        if rand() < devices_rates[:fixed]
+            l = DR.DER.FixedLoad(
+                index=i,
+                T=T,
+                load= γ .* max.(0.0, (load_norm .+ 0.05 .* randn(T)))
             )
-            push!(appliances, ev)
-
-        else
-            # Not an integer number of days
-            # Do not include EV
+            push!(appliances, l)
         end
 
-        # PV
-        ζ = rand(T)
-        pv = DR.DER.CurtailableLoad(
-            index=i,
-            T=T,
-            dt=1.0,
-            load= γ .* PV_norm .* ζ,
-            binaryFlag=true
-        )
-        push!(appliances, pv)
+        # ==================
+        #    Fixed load     
+        # ==================
+        # Dishwasher
+        if rand() < devices_rates[:dishwasher]
+            dishwasher = DR.DER.ShiftableLoad(
+                index = 10000000 + i,  # TODO: handle index number properly
+                T=T,
+                cycle_begin_min=1,
+                cycle_begin_max=T-1,
+                cycle_length=2,
+                cycle_load=[1.0, 1.0]
+            )
+            push!(appliances, dishwasher)
+        end
 
-        # Battery
-        bat = DR.DER.Battery(
-            index=i,
-            T=T,
-            dt=1.0,
-            soc_min=zeros(T),
-            soc_max=13.5 .* ones(T),
-            soc_init=0.0,
-            selfdischargerate=1.0,
-            charge_pwr_min=0.0, charge_pwr_max=5.0,
-            discharge_pwr_min=0.0, discharge_pwr_max=5.0,
-            charge_eff=0.95, discharge_eff=0.95
-        )
-        push!(appliances, bat)
+        # ======================
+        #    Shiftable load     
+        # ======================
+        # Clothes washer
+        if rand() < devices_rates[:clothes_washer]
+            clothes_washer = DR.DER.ShiftableLoad(
+                index = 20000000 + i,  # TODO: handle index number properly
+                T=T,
+                cycle_begin_min=1,
+                cycle_begin_max=T-1,
+                cycle_length=2,
+                cycle_load=[1.0, 1.0]
+            )
+            push!(appliances, clothes_washer)
+
+            # Clothes dryer
+            # Only household with clothes washer may have a clothes dryer
+            if rand() < (
+                devices_rates[:clothes_dryer] / devices_rates[:clothes_washer]
+            )
+
+                clothes_dryer = DR.DER.ShiftableLoad(
+                    index = 30000000 + i,  # TODO: handle index number properly
+                    T=T,
+                    cycle_begin_min=1,
+                    cycle_begin_max=T-2,
+                    cycle_length=3,
+                    cycle_load=[1.0, 1.0, 1.0]
+                )
+                push!(appliances, clothes_dryer)
+            end
+        end
+        # ===================
+        #    Thermal load     
+        # ===================
+        # Electric heating
+        if rand() < devices_rates[:thermal]
+            th = DR.DER.ThermalLoad(
+                index=i, T=T, dt=1.0,
+                temp_min=fill(18.0, T), temp_max=fill(22.0, T),
+                temp_init=20.0,
+                temp_ext=temp_ext + 0.5 .* randn(T),
+                pwr_min=zeros(T), pwr_max=10.0 .* ones(T),
+                C=3.0, η=1.0, μ=0.2,
+                binary_flag=false
+            )
+            push!(appliances, th)
+        end
         
+        # =====================
+        #    PV, EV & ES    
+        # =====================
+        if rand() < devices_rates[:renew]
+
+            # Electric vehicle
+            if (T % 24) == 0
+                ndays = div(T, 24)
+
+                ev = DR.DER.DeferrableLoad(
+                    index=i, T=T, dt=1.0,
+                    num_cycles=ndays,
+                    cycle_begin=[16 + 24*(d-1) for d in 1:ndays],
+                    cycle_end=[24 + 24*(d-1) for d in 1:ndays],
+                    cycle_energy_min=fill(10.0, ndays),
+                    cycle_energy_max=fill(10.0, ndays),
+                    pwr_min=repeat([zeros(15); 1.1*ones(9)], ndays),
+                    pwr_max=repeat([zeros(15); 7.7*ones(9)], ndays),
+                    binary_flag=true
+                )
+                push!(appliances, ev)
+
+            else
+                # Not an integer number of days
+                # Do not include EV
+            end
+
+            # PV
+            ζ = rand(T)
+            pv = DR.DER.CurtailableLoad(
+                index=i,
+                T=T,
+                dt=1.0,
+                load= - γ .* PV_norm .* ζ,
+                binaryFlag=true
+            )
+            push!(appliances, pv)
+
+            # Battery
+            bat = DR.DER.Battery(
+                index=i,
+                T=T,
+                dt=1.0,
+                soc_min=zeros(T),
+                soc_max=13.5 .* ones(T),
+                soc_init=0.0,
+                selfdischargerate=1.0,
+                charge_pwr_min=0.0, charge_pwr_max=5.0,
+                discharge_pwr_min=0.0, discharge_pwr_max=5.0,
+                charge_eff=0.95, discharge_eff=0.95
+            )
+            push!(appliances, bat)
+        end
+
+
         # Household
         h = DR.DER.House(
             index=i,
@@ -303,4 +315,134 @@ function generate_resources(
     end
 
     return resources
+end
+
+function run_experiment(;
+    T0::Int=408+7, T::Int=24, R::Int=1024, ξ::Float64=0.33, seed::Int=42,
+    solvers::Vector{Symbol}=Symbol[],
+    cg_verbose::Bool=true,
+    export_res::Bool=false
+)
+
+    # Load data
+    ont_load = CSV.read("../dat/load2016.csv")
+    ont_price = CSV.read("../dat/price2016.csv")
+    ont_prod = CSV.read("../dat/prod2016.csv")
+    ont_temp = CSV.read("../dat/temperature2016.csv")
+
+    # Market and Time-Of-Use prices
+    p_tou = Vector{Float64}(ont_price.TOU[T0:(T0+T-1)])
+    p_mkt = Vector{Float64}(ont_price.HOEP[T0:(T0+T-1)])
+
+    # Normalized load
+    load_norm = (ont_load.OntDemand / mean(ont_load.OntDemand))[T0:(T0+T-1)]
+
+    # Normalized PV output
+    pv_norm = (ont_prod.SOLAR / mean(ont_prod.SOLAR))[T0:(T0+T-1)]
+
+    # Outside temperature
+    temp_ext = ont_temp.Temperature[T0:(T0+T-1)]
+
+    # Min and max total net load
+    total_load_min = 0.0 * R * ones(T)
+    total_load_max = 6.0 * R * ones(T)
+
+    # Devices' ownership rates
+    own_rates = Dict(
+        :fixed => 1.0,
+        :dishwasher => 0.65,
+        :clothes_washer => 0.9,
+        :clothes_dryer => 0.75,
+        :thermal => 0.6,
+        :renew => ξ
+    )
+
+    # ===========================
+    #   Instanciate resources
+    # ===========================
+    resources = generate_resources(
+        R, T, own_rates,
+        load_norm, pv_norm, temp_ext, p_mkt,
+        seed=seed
+    )
+
+    GRBENV = Gurobi.Env()
+    GRBSolverSP = GurobiSolver(
+        GRBENV, # Re-use the same Gurobi environment
+        OutputFlag=0,
+        Threads=1,
+        # Tuned parameters
+        MIPFocus=1,
+        Presolve=-1
+    )
+
+    pool = Linda.Oracle.LindaOraclePool(
+        [
+            Linda.Oracle.LindaOracleMIP(r, GRBSolverSP)
+            for r in resources
+        ]
+    )
+
+    # Generate one initial column for each resource
+    initial_cols = Linda.Column[]
+    for o in pool.oracles
+        Linda.Oracle.query!(o, zeros(2*T), Inf)
+        col = Linda.Oracle.get_new_columns(o)[1]
+        push!(initial_cols, col)
+    end
+
+    # ===========================
+    #   Benchmark RMP solvers
+    # ===========================
+
+    env = Linda.LindaEnv()
+    env[:verbose] = cg_verbose
+    env[:num_cgiter_max] = 250
+    env[:num_columns_max] = Int(ceil(R/10))
+
+    logs = Dict{Symbol, Any}()
+
+    for s in solvers
+        
+        # Instanciate MP
+        mp_solver = solver(s)
+        mp = generate_mp(R, T, total_load_min, total_load_max, initial_cols, mp_solver)
+
+        # Log
+        cg_log = Dict{Symbol, Any}(
+            :T => T,
+            :T0 => T0,
+            :R => R,
+            :seed => seed,
+            :xi => ξ,
+            :solver => s
+        )
+
+        if cg_verbose
+            println("*"^80, "\n")
+            println("Problem statistics:")
+            println("\tR    = $R")
+            println("\tT    = $T")
+            println("\tξ    = $ξ")
+            println("\tseed = $seed")
+            println("\tRMP  = $s")
+            println("\tItermax = ", env[:num_cgiter_max])
+            println("\tColmax = ", env[:num_columns_max])
+            println()
+        end
+
+        Linda.solve_colgen!(env, mp, pool, cg_log=cg_log)
+
+        logs[s] = cg_log
+        
+        # Print output
+        if export_res
+            df = DataFrames.DataFrame(cg_log)
+            CSV.write("res_$(T)_$(R)_$(ξ)_$(seed)_$(s).csv", df)
+        end
+
+    end
+
+    return logs
+
 end
---
+++ exp/solvers.jl
@@ -0,0 +1,145 @@
+import MathProgBase
+const MPB = MathProgBase
+
+# Import solvers
+import Linda
+import Tulip
+import Mosek
+import Gurobi
+import CPLEX
+import Clp
+
+import CPLEX:CplexSolver
+import Gurobi: GurobiSolver
+import Mosek: MosekSolver
+import Clp:ClpSolver
+import Tulip.TulipSolver
+
+
+# Extend MathProgBase interface
+MPB.getbarrieriter(m::CPLEX.CplexMathProgModel) = CPLEX.@cpx_ccall(getbaritcnt, Cint, (Ptr{Cvoid}, Ptr{Cvoid}), m.inner.env, m.inner.lp)
+MPB.getsimplexiter(m::CPLEX.CplexMathProgModel) = CPLEX.@cpx_ccall(getitcnt, Cint, (Ptr{Cvoid}, Ptr{Cvoid}), m.inner.env, m.inner.lp)
+
+MPB.getsimplexiter(m::Gurobi.GurobiMathProgModel) = Gurobi.get_dblattr(m.inner, "IterCount")
+MPB.getbarrieriter(m::Gurobi.GurobiMathProgModel) = Gurobi.get_intattr(m.inner, "BarIterCount")
+
+MPB.getbarrieriter(m::Mosek.MosekMathProgSolverInterface.MosekLinearQuadraticModel) = Mosek.getintinf(m.task, Mosek.MSK_IINF_INTPNT_ITER)
+MPB.getsimplexiter(m::Mosek.MosekMathProgSolverInterface.MosekLinearQuadraticModel) = Mosek.getintinf(m.task,Mosek.MSK_IINF_SIM_PRIMAL_ITER)+Mosek.getintinf(m.task,Mosek.MSK_IINF_SIM_DUAL_ITER)
+
+MPB.getsimplexiter(m::Tulip.TulipMathProgModel) = 0
+
+MPB.getsimplexiter(m::Clp.ClpMathProgSolverInterface.ClpMathProgModel) = Clp.number_iterations(m.inner)
+MPB.getbarrieriter(m::Clp.ClpMathProgSolverInterface.ClpMathProgModel) = Clp.number_iterations(m.inner)
+
+# Solver instanciation
+
+solver(s::Symbol) = solver(Val(s))
+solver(::Val{:TLP}) = generate_lpsolver(:TLP)
+solver(::Val{:MSK}) = generate_lpsolver(:MSK, lpmethod=4, crossover=false)
+solver(::Val{:GRB}) = generate_lpsolver(:GRB, lpmethod=4, crossover=false)
+solver(::Val{:CPX}) = generate_lpsolver(:CPX, lpmethod=4, crossover=false)
+solver(::Val{:SGRB}) = generate_lpsolver(:GRB, lpmethod=0, crossover=true)
+solver(::Val{:SCPX}) = generate_lpsolver(:CPX, lpmethod=0, crossover=true)
+
+
+"""
+    generate_lpsolver()
+
+Instanciate an LP solver for the RMP.
+
+# Arguments
+- `name`: Name of the solver
+- `verbose`: verbosity level. 0 means no output.
+- `lpmethod`: indicates which algorithm to use.
+    0 is default, 1 is primal simplex, 2 is dual simplex, 3 is simplex, 4 is barrier
+- `nthreads`: number of threads available to the solver
+- `crossover`: whether to perform crossover.
+    0 is default, 1 is yes, 2 is no
+- `presolve`: whether to perform presolve
+    0 is no, 1 is yes
+"""
+generate_lpsolver(
+    name::Symbol;
+    verbose::Int=0,
+    lpmethod::Int=0,
+    nthreads=1,
+    crossover::Bool=false,
+    presolve::Int=0
+) = generate_lpsolver(Val(name), verbose, lpmethod, nthreads, crossover, presolve)
+
+
+function generate_lpsolver(::Val{:CPX},
+    _verbose, _lpmethod, _nthreads, _crossover, _presolve
+)
+
+    # crossover parameter
+    if _lpmethod == 4 
+        crossover = _crossover ? 1 : 2
+    else
+        crossover = 0
+    end
+
+    CplexSolver(
+        CPX_PARAM_SCRIND=_verbose,
+        CPX_PARAM_PREIND=_presolve,
+        CPX_PARAM_THREADS=_nthreads,
+        CPX_PARAM_LPMETHOD=_lpmethod, # 0:default, 1:PS, 2:DS, 4:IPM, 6:Concurrent
+        CPX_PARAM_SOLUTIONTYPE=crossover  # 0: default, 1: basic, 2:no crossover
+    )
+
+end
+
+function generate_lpsolver(::Val{:GRB},
+    _verbose, _lpmethod, _nthreads, _crossover, _presolve
+)
+
+    # crossover parameter
+    crossover = _crossover ? 1 : 0
+
+    if _lpmethod == 1 || _lpmethod == 2
+        lpmethod = _lpmethod - 1  # simplex
+    elseif _lpmethod == 4
+        lpmethod = 2  # barrier
+    else
+        lpmethod = -1  # default setting
+    end
+
+    GurobiSolver(
+        OutputFlag=_verbose,
+        Method=lpmethod,
+        Presolve=_presolve,
+        Threads=_nthreads,
+        Crossover=crossover,
+        InfUnbdInfo=1  # to get infeasible rays if needed
+    )
+
+end
+
+function generate_lpsolver(::Val{:MSK},
+    _verbose, _lpmethod, _nthreads, _crossover, _presolve
+)
+    if _lpmethod == 1
+        optimizer = 6  # primal simplex
+    elseif _lpmethod == 2
+        optimizer = 1  # dual simplex
+    elseif _lpmethod == 3
+        optimizer = 3  # any simplex
+    elseif _lpmethod == 4
+        optimizer = 4  # interior-point
+    else
+        optimizer = 2  # default
+    end
+    MosekSolver(
+        MSK_IPAR_LOG=_verbose,
+        MSK_IPAR_NUM_THREADS=_nthreads,
+        MSK_IPAR_OPTIMIZER=optimizer,
+        MSK_IPAR_PRESOLVE_USE=_presolve,
+        MSK_IPAR_INTPNT_BASIS=(1*_crossover),  # crossover
+        # MSK_IPAR_INTPNT_MAX_NUM_COR=-1  # default
+    )
+
+end
+
+generate_lpsolver(::Val{:TLP},
+    _verbose, _lpmethod, _nthreads, _crossover, _presolve
+) = TulipSolver(verbose = _verbose)
